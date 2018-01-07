d07d1c5d2b2876eade509c8230bbe6fe27baddb4
Alex Nikitin <alex@nikitin.ninja>
2018-01-07 10:27:18 -0800

    minor upd

--- entryPoint.sh
+++ entryPoint.sh
@@ -5,4 +5,4 @@ SITE_URL="https://losangeles.craigslist.org/search/cta?hasPic=1&search_distance=
 printf "$SITE_URL" > /var/www/html/tmp/site-url.txt
 xvfb-run --auto-servernum phantomjs --version > /var/www/html/tmp/phantomjs-version.txt
 xvfb-run --auto-servernum phantomjs src/phantomjs/rasterize.js "$SITE_URL" "/var/www/html/tmp/results.pdf" "letter"
-php src/php/craigslist-parser-cars.php > /var/www/html/tmp/results-scraping.txt
+php src/php/craigslist-parser-cars.php > /var/www/html/tmp/results-scraping.json
